5edc62593fcef3e618c299e616870bde866cf9e3
umarkhemis <umarkhemis9@gmail.com>
2026-03-31 17:13:47 +0300

    Modified the model/user

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -32,6 +32,7 @@ class User(UUIDPrimaryKeyMixin, TimestampMixin, Base):
 
     refresh_tokens = relationship("RefreshToken", back_populates="user", cascade="all, delete-orphan")
     cultural_site = relationship("CulturalSite", back_populates="user", uselist=False)
+    bookings = relationship("Booking", back_populates="tourist", cascade="all, delete-orphan", foreign_keys="Booking.tourist_id")
 
     __table_args__ = (
         Index("ix_users_role", "role"),
